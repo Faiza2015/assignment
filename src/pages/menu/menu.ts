e6ac3a6fbb614a7b24be9cc37fe259096fdb3fe3
FaizaR <faiza.rahat@hotmail.com>
2017-12-18 10:20:01 +0500

    FAB Favorites

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../../providers/dish/dish';
 import { DishdetailPage } from '../dishdetail/dishdetail';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
+
 
 /**
  * Generated class for the MenuPage page.
@@ -21,7 +23,14 @@ export class MenuPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private dishservice: DishProvider,
+    private favoriteservice: FavoriteProvider,
     @Inject('BaseURL') private BaseURL) { }
+	
+	
+	  addToFavorites(dish: Dish) {
+    console.log('Adding to Favorites', dish.id);
+    this.favoriteservice.addFavorite(dish.id);
+  }
   
   ngOnInit() {
     this.dishservice.getDishes()
