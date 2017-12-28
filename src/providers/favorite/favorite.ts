09b592232b4a2c3e2841139b37e731eb8c1d1f98
FaizaR <faiza.rahat@hotmail.com>
2017-12-28 15:51:49 +0500

    Assignment 3

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
+import { Favorite } from '../../shared/favorite';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -14,16 +16,19 @@ import { DishProvider } from '../dish/dish';
 export class FavoriteProvider {
 
   favorites: Array<any>;
-
-  constructor(public http: Http,  private dishservice: DishProvider) {
+favorite: Favorite={favorites: []};
+  constructor(public http: Http,  private dishservice: DishProvider,private storage: Storage) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
+	 
+
+     
   }
 
  addFavorite(id: number): boolean {
     if (!this.isFavorite(id))
       this.favorites.push(id);
-    console.log('favorites', this.favorites);
+	  this.storage.set('favorites',this.favorites);
     return true;
   }
 
@@ -34,6 +39,7 @@ deleteFavorite(id: number): Observable<Dish[]> {
     let index = this.favorites.indexOf(id);
     if (index >= 0) {
       this.favorites.splice(index,1);
+	  this.storage.set('favorites',this.favorites)
       return this.getFavorites();
     }
     else {
@@ -43,7 +49,21 @@ deleteFavorite(id: number): Observable<Dish[]> {
   }
   
   getFavorites(): Observable<Dish[]> {
-    return this.dishservice.getDishes()
+
+	   this.storage.get('favorites').then(favorites => {
+        if (favorites) {
+          console.log(favorites+'Hellooooo');
+          this.favorites = favorites;
+		   
+        }
+        else
+          console.log('favorites not defined');
+      });
+	  return this.dishservice.getDishes()
       .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+         
+	  
+	
+
   }
 }
---
+++ src/shared/favorite.ts
@@ -0,0 +1,5 @@
+
+
+export interface Favorite {
+   favorites:Array<any>;
+};
